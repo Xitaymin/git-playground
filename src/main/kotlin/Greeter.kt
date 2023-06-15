f773d5fc075579d08266ff59efc8c2cfe93c4447
Yaroslav Barashenko <y.barashenko@gmail.com>
2023-06-15 20:14:40 +0300

    Test 6 1 change

--- src/main/kotlin/Greeter.kt
+++ src/main/kotlin/Greeter.kt
@@ -2,4 +2,8 @@ object Greeter {
     fun sayHello(name: String) {
         println("Hello, $name)")
     }
+
+    fun sayGoodbye(name: String) {
+        println("Goodbye, $name")
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
 fun main() {
     Greeter.sayHello("my friends")
+    Greeter.sayGoodbye("my friends")
 }
